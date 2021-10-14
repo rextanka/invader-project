05a8d8259b4a087b515a7facb5dcdcf9b04ad7b0
Nick Thompson <nickt@me.com>
2021-10-14 15:14:03 -0700

    Set up rocket base, keep it in screen bounds and let user move with keys

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+function setupRocketBase () {
+    rocketBase = sprites.create(assets.image`missileBase`, SpriteKind.Player)
+    rocketBase.setPosition(Math.trunc(scene.screenWidth() / 2), scene.screenHeight() - rocketBase.height)
+    rocketBase.setStayInScreen(true)
+    controller.moveSprite(rocketBase, 100, 0)
+}
+let rocketBase: Sprite = null
+setupRocketBase()
